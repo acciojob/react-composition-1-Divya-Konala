91f6bc7034d55844dcd82ae8115afbd52d10693f
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-04-13 15:55:49 +0530

    tabs

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+import Tab from "./Tab";
+const tabs=["Tab 1","Tab 2","Tab 3"];
 const App = () => {
+  const [currTab,setCurrTab]=useState();
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          {
+            tabs.map((tab)=>{
+              return <li onClick={()=>setCurrTab(tab)}>{tab}</li>
+            })
+          }
+        </ul>
+        {
+          (currTab!==undefined && <Tab tab={currTab} />)
+        }
     </div>
   )
 }
---
+++ src/components/Tab.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tab=({tab})=>{
+    return <div className='tab'>
+        <p>This is the content for {tab}</p>
+    </div>
+}
+
+export default Tab
